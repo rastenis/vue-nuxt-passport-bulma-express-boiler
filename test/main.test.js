99bec971f2ee4e96a4b9bbe17bd267565dc6abcf
Matas R <matas2k@gmail.com>
2018-07-30 15:10:43 +0300

    proper config for test building

--- test/main.test.js
+++ test/main.test.js
@@ -1,31 +1,29 @@
-import { resolve } from 'path'
-import { Nuxt, Builder } from 'nuxt-edge'
-import { JSDOM } from 'jsdom'
-import test from 'ava'
+import { resolve } from "path";
+import { Nuxt, Builder } from "nuxt-edge";
+import { JSDOM } from "jsdom";
+import test from "ava";
 
 // We keep the nuxt and server instance
 // So we can close them at the end of the test
-let nuxt = null
+let nuxt = null;
 
 // Init Nuxt.js and create a server listening on localhost:4000
 test.before(async () => {
-  const config = {
-    dev: false,
-    rootDir: resolve(__dirname, '..')
-  }
-  nuxt = new Nuxt(config)
-  await new Builder(nuxt).build()
-  await nuxt.listen(4000, 'localhost')
-}, 30000)
+  const nuxtConfig = require("../nuxt.config.js");
+
+  nuxt = new Nuxt(nuxtConfig);
+  await new Builder(nuxt).build();
+  await nuxt.listen(4000, "localhost");
+}, 30000);
 
 // Testing contents of publicly accessible routes
-test('Index route & its contents', async testing => {
-  const context = {}
-  const { index } = await nuxt.renderRoute('/', context)
-  testing.true(index.includes('<h1 class="title">WELCOME</h1>'))
-})
+test("Index route & its contents", async testing => {
+  const context = {};
+  const { index } = await nuxt.renderRoute("/", context);
+  testing.true(index.includes('<h1 class="title">WELCOME</h1>'));
+});
 
 // ending the test
-test.after('Exitting...', t => {
-  nuxt.close()
-})
+test.after("Exitting...", t => {
+  nuxt.close();
+});
