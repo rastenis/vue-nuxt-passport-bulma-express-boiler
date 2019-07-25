08efcf57953f3b8bb86bfc5549c5e02f3d7d97d6
Matas R <matas2k@gmail.com>
2019-07-25 15:10:02 +0300

    cookie persistence fix

--- test/index.test.js
+++ test/index.test.js
@@ -1,40 +1,41 @@
-import { resolve } from 'path'
-import { Nuxt, Builder } from 'nuxt'
-import test from 'ava'
-import axios from 'axios'
-import config from '../config/config.json'
+import { resolve } from "path";
+import { Nuxt, Builder } from "nuxt";
+import test from "ava";
+import axios from "axios";
+import config from "../config/config.json";
 
 // We keep the nuxt and server instance
 // So we can close them at the end of the test
-let nuxt = null
+let nuxt = null,
+  cookie,
+  ax = axios.create({ withCredentials: true });
 
 // Init Nuxt.js and create a server listening on localhost:4000
 test.before(async () => {
-
   const nuxtConfig = {
     dev: false,
-    rootDir: resolve(__dirname, '..'),
+    rootDir: resolve(__dirname, ".."),
     css: ["~/assets/css/main.css", "~/assets/css/bulma.min.css"],
     serverMiddleware: [
       // API middleware
       "~/src/server.js"
     ]
-  }
-  nuxt = new Nuxt(nuxtConfig)
-  await new Builder(nuxt).build()
-  await nuxt.server.listen(4000, 'localhost')
-}, 30000)
+  };
+  nuxt = new Nuxt(nuxtConfig);
+  await new Builder(nuxt).build();
+  await nuxt.server.listen(4000, "localhost");
+}, 30000);
 
 // Example of testing only generated html
-test('Index route HTML contents', async (t) => {
-  const context = {}
-  const { html } = await nuxt.server.renderRoute('/', context)
-  t.true(html.includes('Log in to see users'))
-  t.true(html.includes('WELCOME'))
+test("Index route HTML contents", async t => {
+  const context = {};
+  const { html } = await nuxt.server.renderRoute("/", context);
+  t.true(html.includes("Log in to see users"));
+  t.true(html.includes("WELCOME"));
 
   // nuxt bundles images, so the way to test this is just via alts and class
-  t.true(html.includes('alt="Nuxt.js Logo" class="logo"'))
-})
+  t.true(html.includes('alt="Nuxt.js Logo" class="logo"'));
+});
 
 // Testing register route
 test("Register route & its contents", async t => {
@@ -63,16 +64,59 @@ test("Non-existent page fetch", async t => {
 });
 
 test("Auth-gated fetch attempt", async t => {
-  try{
-    const { data } = await axios.get(`http://localhost:${config.port}/api/users`);
-  }catch(e){
-    t.is(e.response.status,403);
-    t.is(e.response.statusText,"Forbidden");
-    t.is(e.response.data,{ success: false, message: 'Auth needed.' } );
+  try {
+    const { data } = await ax.get(`http://localhost:${config.port}/api/users`);
+  } catch (e) {
+    t.is(e.response.status, 403);
+    t.is(e.response.statusText, "Forbidden");
+    t.deepEqual(e.response.data, { success: false, message: "Auth needed." });
+  }
+});
+
+test("Registration", async t => {
+  try {
+    const { data, headers } = await ax.post(
+      `http://localhost:${config.port}/register`,
+      {
+        email: `tester${Math.random()
+          .toString(36)
+          .substr(2, 7)}@test.com`,
+        password: "password"
+      },
+      { withCredentials: true }
+    );
+    t.deepEqual(data.meta, {
+      error: false,
+      msg: "You have successfully registered!"
+    });
+    cookie = headers["set-cookie"][0];
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+test("Auth-gated fetch attempt with auth", async t => {
+  try {
+    const { data } = await ax({
+      method: "get",
+      url: `http://localhost:${config.port}/api/users`,
+      withCredentials: true,
+      headers: {
+        Cookie: cookie
+      }
+    });
+
+    t.deepEqual(data, [
+      { name: "Alexandre" },
+      { name: "Pooya" },
+      { name: "Sébastien" }
+    ]);
+  } catch (e) {
+    console.log(e);
   }
 });
 
 // Close server and ask nuxt to stop listening to file changes
-test.after('Closing server and nuxt.js', (t) => {
-  nuxt.close()
-})
+test.after("Closing server and nuxt.js", t => {
+  nuxt.close();
+});
